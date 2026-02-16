f70cc20afb4d310589925aec6855a8f519ecd05e
Anna Mlodzianowska <an.mlodzianowska@gmail.com>
2026-02-15 19:03:29 -0600

    Add SwaggerUI

--- FraudDetection.Api/Program.cs
+++ FraudDetection.Api/Program.cs
@@ -6,12 +6,15 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 //app.UseHttpsRedirection();
